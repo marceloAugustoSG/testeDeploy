1216a9649abafea05fa259c735b0f52f9b22865f
MarceloASG <marcelo_augusto.soares@hotmail.com>
2023-09-07 14:45:53 -0300

    add host in app.listem

--- src/server.js
+++ src/server.js
@@ -1,19 +1,17 @@
 import Express from "express"
 import { prisma } from '../src/PrismaClient.js'
+
 const app = Express()
-
-
 
 app.get("/users", async (req, res) => {
     const user = await prisma.user.findMany()
     return res.status(200).json(user)
 
 })
+app.listen(() => {
 
+    host: '0.0.0.0'
 
-
-app.listen({
-    host: '0.0.0.0',
-    port: process.env.PORT
+        // console.log('rodando na porta 3000' + process.env.DATABASE_URL)
 
 })
